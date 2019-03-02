5aa5b770b864d33685a7942780d0566161b22e97
Michel Motta da Silva <michelmottadasilva@gmail.com>
2019-03-02 13:45:00 -0400

    Adicionado componente Order By

---
+++ src/components/OrderBy.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9"></div>
+            <div class="col-md-3">
+                <div class="input-group sort">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">Order By</label>
+                    </div>
+                    <select @change="onSelectChange($event)" class="custom-select glowing-border" id="inputGroupSelect01">
+                        <option value="null" selected>Choose...</option>
+                        <option value="name">Name</option>
+                        <option value="date">Date</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OrderBy',
+    methods: {
+        onSelectChange(event) {
+            if(event.target.value != 'null'){
+                this.$store.dispatch("orderMoviesBy", event.target.value)
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.sort{
+    margin-top: 20px;
+}
+.glowing-border {
+    border: 2px solid #dadada;
+    border-radius: 7px;
+}
+.glowing-border:focus { 
+    outline: none;
+    border-color: #fff;
+    box-shadow: 0 0 30px #00eeff;
+}    
+</style>
