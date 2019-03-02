89387dbb0c968c3346c00c869d2801c721698578
Michel Motta da Silva <michelmottadasilva@gmail.com>
2019-03-02 01:20:15 -0400

    Adicionado componente Search a alterações para correto funcionamento

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   	<div id="app">
     	<div class="container-fluid">
 			<Header></Header>
+			<Search></Search>
       		<Movies></Movies>
 			<Pagination></Pagination>
 			<Footer></Footer>
@@ -11,6 +12,7 @@
 
 <script>
 import Header from "./components/Header"
+import Search from "./components/Search"
 import Movies from "./components/Movies"
 import Pagination from "./components/Pagination"
 import Footer from "./components/Footer"
@@ -20,6 +22,7 @@ export default {
   	name: "app",
   	components: {
 		Header,
+		Search,
 		Movies,
 		Pagination,
 		Footer
---
+++ src/components/Search.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <form class="card card-sm">
+                    <div class="card-body row no-gutters align-items-center">
+                        <div class="col-auto">
+                            <i class="fa fa-search h4 text-body"></i>
+                        </div>
+                        <div class="col">
+                            <input v-model="searchTerm" class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search Star Wars movies by name">
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-lg btn-dark btn-black" type="submit" @click.prevent="searchMovieByName()">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    data() {
+        return {
+            searchTerm: ''
+        }
+    },
+    methods: {
+        searchMovieByName() {
+            this.$store.dispatch("loadMovies", 'https://swapi.co/api/films/?search=' + this.searchTerm)
+        }
+    }
+}
+</script>
+
+<style>
+.card{
+    border-radius: 7px;
+}
+.form-control-borderless {
+    border: none;
+}
+.form-control-borderless:hover, .form-control-borderless:active, .form-control-borderless:focus {
+    border: none;
+    outline: none;
+    box-shadow: none;
+}
+.fa-search{
+    font-size: 30px;
+}
+.btn-black{
+    background: black;
+}
+</style>
