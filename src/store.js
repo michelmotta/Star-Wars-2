e92b740de77fdcf284d8fdbd539fd51a36581989
Michel Motta da Silva <michelmottadasilva@gmail.com>
2019-03-01 22:55:32 -0400

    First commit

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import vue from 'vue'
+import vuex from 'vuex'
+import axios from 'axios'
+import Vue from 'vue'
+
+Vue.use(vuex, axios)
+
+export default new vuex.Store({
+    state: {
+        teste: "Teste 123..."
+    }
+})
